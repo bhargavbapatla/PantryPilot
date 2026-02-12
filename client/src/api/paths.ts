a5554894083bb8d5a6c98f0254146bfb0cc69c89
bhargavbapatla <bhargav.bapatla20@gmail.com>
2026-02-12 13:20:28 +0530

    deleted client

--- client/src/api/paths.ts
+++
@@ -1,8 +0,0 @@
-export const userRegister = '/auth/register';
-export const userLogin = '/auth/login';
-export const userGoogleLogin = '/auth/googleLogin';
-export const Inventory = '/inventory';
-export const Product = '/products';
-export const orders = '/orders';
-export const customer = '/customers';
-export const ai = '/ai';
--- client/src/api/users.ts
+++
@@ -1,6 +0,0 @@
-import { authorizedAPI } from './api';
-
-export const testingDataGet = async() => {
-    const response = await authorizedAPI.get('/user/test');
-    return response.data;
-}
